5c9f80c1be7afb9b582dd9caa36fb899e7f47c1f
Colin Woon <colin.wyj@gmail.com>
2025-09-10 18:07:38 +0800

    chore: convert spaces to tab

--- models/networking/ServerLoop/PollHelpers.cpp
+++ models/networking/ServerLoop/PollHelpers.cpp
@@ -1,13 +1,15 @@
 #include "PollHelpers.hpp"
 
-size_t indexOf(std::map<int, size_t>& fdIndex, int fd){
+size_t indexOf(std::map<int, size_t> &fdIndex, int fd)
+{
 	std::map<int, size_t>::iterator itr = fdIndex.find(fd);
 	if (itr == fdIndex.end())
 		return (SIZE_MAX);
 	return (itr->second);
 }
 
-void addPollFd(std::vector<pollfd>& pollFdList, std::map<int, size_t>& fdIndex, int fd, short events){
+void addPollFd(std::vector<pollfd> &pollFdList, std::map<int, size_t> &fdIndex, int fd, short events)
+{
 	struct pollfd pollFd;
 
 	pollFd.fd = fd;
@@ -17,33 +19,37 @@ void addPollFd(std::vector<pollfd>& pollFdList, std::map<int, size_t>& fdIndex, 
 	fdIndex[fd] = pollFdList.size() - 1;
 }
 
-void modifyEvent(std::vector<pollfd>& pollFdList, std::map<int, size_t>& fdIndex, int fd, short events){
+void modifyEvent(std::vector<pollfd> &pollFdList, std::map<int, size_t> &fdIndex, int fd, short events)
+{
 	size_t i = indexOf(fdIndex, fd);
 	if (i != SIZE_MAX)
 		pollFdList[i].events = events;
 }
 
-void delPollFd(std::vector<pollfd>& pollFdList, std::map<int, size_t>& fdIndex, int fd){
+void delPollFd(std::vector<pollfd> &pollFdList, std::map<int, size_t> &fdIndex, int fd)
+{
 	std::map<int, size_t>::iterator itr = fdIndex.find(fd);
 	if (itr == fdIndex.end())
 		return;
 
-    size_t i = itr->second;
-    size_t last = pollFdList.size() - 1;
+	size_t i = itr->second;
+	size_t last = pollFdList.size() - 1;
 
-    if (i != last) {
-        pollFdList[i] = pollFdList[last];
-        fdIndex[pollFdList[i].fd] = i;
-    }
-    pollFdList.pop_back();
-    fdIndex.erase(itr);
+	if (i != last)
+	{
+		pollFdList[i] = pollFdList[last];
+		fdIndex[pollFdList[i].fd] = i;
+	}
+	pollFdList.pop_back();
+	fdIndex.erase(itr);
 }
 
-bool listenerOrNot(std::vector<int>& listenerFdList, int fd){
-	for (size_t i = 0; i < listenerFdList.size(); i++){
+bool listenerOrNot(std::vector<int> &listenerFdList, int fd)
+{
+	for (size_t i = 0; i < listenerFdList.size(); i++)
+	{
 		if (fd == listenerFdList[i])
 			return true;
 	}
 	return false;
 }
-
--- models/networking/ServerLoop/ServerLoop.cpp
+++ models/networking/ServerLoop/ServerLoop.cpp
@@ -3,11 +3,12 @@
 #include "ServerHelpers.hpp"
 #include "Timeout.hpp"
 
-Client::Client():fd(-1), closeFlag(false), timeoutMs(0), expiresAtMs(0), responseQueued(false){}
+Client::Client() : fd(-1), closeFlag(false), timeoutMs(0), expiresAtMs(0), responseQueued(false) {}
 
-Client::~Client(){}
+Client::~Client() {}
 
-void mainServerLoop(std::vector<int>& listenerFdList, std::vector<Server>& serverList){
+void mainServerLoop(std::vector<int> &listenerFdList, std::vector<Server> &serverList)
+{
 	std::vector<pollfd> pollFdList;
 	std::map<int, size_t> fdIndex;
 	std::map<int, Client> clientList;
@@ -16,58 +17,66 @@ void mainServerLoop(std::vector<int>& listenerFdList, std::vector<Server>& serve
 
 	for (size_t i = 0; i < listenerFdList.size(); i++)
 		addPollFd(pollFdList, fdIndex, listenerFdList[i], POLLIN);
-	while (1){
-		int timeoutMs = calcNextTimeout(clientList, 1000); 
+	while (1)
+	{
+		int timeoutMs = calcNextTimeout(clientList, 1000);
 		int pollReady = poll(&pollFdList[0], pollFdList.size(), timeoutMs);
-		if (pollReady == -1){
+		if (pollReady == -1)
+		{
 			if (errno == EINTR)
 				continue;
 			throw std::runtime_error("poll error.");
 		}
-		if (pollReady == 0){
+		if (pollReady == 0)
+		{
 			closeExpiredClients(pollFdList, fdIndex, clientList);
 			continue;
 		}
-        std::vector<int> errFds;
-        std::vector<int> readyListeners;
-        std::vector<int> readyReaders;
-        std::vector<int> readyWriters;
+		std::vector<int> errFds;
+		std::vector<int> readyListeners;
+		std::vector<int> readyReaders;
+		std::vector<int> readyWriters;
 
-        size_t count = pollFdList.size();
-        for (size_t i = 0; i < count; ++i) {
-            struct pollfd p = pollFdList[i];
-            if (p.revents == 0)
+		size_t count = pollFdList.size();
+		for (size_t i = 0; i < count; ++i)
+		{
+			struct pollfd p = pollFdList[i];
+			if (p.revents == 0)
 				continue;
-            if ((p.revents & POLLERR) != 0 || (p.revents & POLLHUP) != 0 || (p.revents & POLLNVAL) != 0){
+			if ((p.revents & POLLERR) != 0 || (p.revents & POLLHUP) != 0 || (p.revents & POLLNVAL) != 0)
+			{
 				if (!listenerOrNot(listenerFdList, p.fd))
-                    errFds.push_back(p.fd);
+					errFds.push_back(p.fd);
 				else
-                    std::cerr << "listener error-ish revents on fd " << p.fd << "\n";
-                continue;
-            }
-            if (listenerOrNot(listenerFdList, p.fd)) {
-                if ((p.revents & POLLIN) != 0)
-                    readyListeners.push_back(p.fd);
-                continue;
-            }
-            if (clientList.find(p.fd) != clientList.end()){
-                if ((p.revents & POLLOUT) != 0)
-                    readyWriters.push_back(p.fd);
+					std::cerr << "listener error-ish revents on fd " << p.fd << "\n";
+				continue;
+			}
+			if (listenerOrNot(listenerFdList, p.fd))
+			{
 				if ((p.revents & POLLIN) != 0)
-                    readyReaders.push_back(p.fd);
-            }
-        }
-        for (size_t i = 0; i < errFds.size(); ++i)
-            closeClient(errFds[i], pollFdList, fdIndex, clientList);
-        for (size_t i = 0; i < readyListeners.size(); ++i)
-            acceptClients(readyListeners[i], pollFdList, fdIndex, clientList, timeoutList);
-        for (size_t i = 0; i < readyWriters.size(); ++i)
-            if (clientList.find(readyWriters[i]) != clientList.end())
-                writeOnce(readyWriters[i], pollFdList, fdIndex, clientList);
-        for (size_t i = 0; i < readyReaders.size(); ++i) {
-            int fd = readyReaders[i];
-            if (clientList.find(fd) != clientList.end())
-                readOnce(fd, pollFdList, fdIndex, clientList);
+					readyListeners.push_back(p.fd);
+				continue;
+			}
+			if (clientList.find(p.fd) != clientList.end())
+			{
+				if ((p.revents & POLLOUT) != 0)
+					readyWriters.push_back(p.fd);
+				if ((p.revents & POLLIN) != 0)
+					readyReaders.push_back(p.fd);
+			}
+		}
+		for (size_t i = 0; i < errFds.size(); ++i)
+			closeClient(errFds[i], pollFdList, fdIndex, clientList);
+		for (size_t i = 0; i < readyListeners.size(); ++i)
+			acceptClients(readyListeners[i], pollFdList, fdIndex, clientList, timeoutList);
+		for (size_t i = 0; i < readyWriters.size(); ++i)
+			if (clientList.find(readyWriters[i]) != clientList.end())
+				writeOnce(readyWriters[i], pollFdList, fdIndex, clientList);
+		for (size_t i = 0; i < readyReaders.size(); ++i)
+		{
+			int fd = readyReaders[i];
+			if (clientList.find(fd) != clientList.end())
+				readOnce(fd, pollFdList, fdIndex, clientList);
 		}
 		closeExpiredClients(pollFdList, fdIndex, clientList);
 	}
